111db8efe1042d0a2c8be83bc65015ee204201c9
zengguankai <zkg725@163.com>
2023-05-06 03:52:08 +0800

    test config and dependency ok

---
+++ src/main/java/com/experiment/ecrecommendersystem/EcRecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.ecrecommendersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcRecommenderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcRecommenderSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/experiment/ecrecommendersystem/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.experiment.ecrecommendersystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("/user")
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
+}
